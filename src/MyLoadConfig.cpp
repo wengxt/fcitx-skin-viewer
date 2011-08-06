cb883ade07194f29a7698e205d2a752198660862
Weng Xuetian <wengxt@gmail.com>
2011-08-06 21:05:59 +0800

    dont use hand made parser

--- src/MyLoadConfig.cpp
+++ src/MyLoadConfig.cpp
@@ -1,141 +1,169 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
 #include "MyLoadConfig.h"
+#include <fcitx-config/xdg.h>
+#include <fcitx-utils/log.h>
+#include <libintl.h>
+#include <limits.h>
+#include "config.h"
 
-#define MID 64
-#define LONG 128
+#define _(x) gettext(x)
 
-using namespace std;
+static void FilterPlacement(GenericConfig* config, ConfigGroup *group, ConfigOption *option, void* value, ConfigSync sync, void* arg);
+static void ParsePlacement(UT_array* sps, char* placment);
 
-_GeneralParseStruct::_GeneralParseStruct()
+static const UT_icd place_icd = {sizeof(SkinPlacement), NULL, NULL, NULL };
+
+CONFIG_BINDING_BEGIN(FcitxSkin)
+CONFIG_BINDING_REGISTER("SkinInfo","Name",skinInfo.skinName)
+CONFIG_BINDING_REGISTER("SkinInfo","Version",skinInfo.skinVersion)
+CONFIG_BINDING_REGISTER("SkinInfo","Author",skinInfo.skinAuthor)
+CONFIG_BINDING_REGISTER("SkinInfo","Desc",skinInfo.skinDesc)
+    
+CONFIG_BINDING_REGISTER("SkinFont","FontSize",skinFont.fontSize)
+CONFIG_BINDING_REGISTER("SkinFont","MenuFontSize",skinFont.menuFontSize)
+CONFIG_BINDING_REGISTER("SkinFont","TipColor",skinFont.fontColor[MSG_TIPS])
+CONFIG_BINDING_REGISTER("SkinFont","InputColor",skinFont.fontColor[MSG_INPUT])
+CONFIG_BINDING_REGISTER("SkinFont","IndexColor",skinFont.fontColor[MSG_INDEX])
+CONFIG_BINDING_REGISTER("SkinFont","UserPhraseColor",skinFont.fontColor[MSG_USERPHR])
+CONFIG_BINDING_REGISTER("SkinFont","FirstCandColor",skinFont.fontColor[MSG_FIRSTCAND])
+CONFIG_BINDING_REGISTER("SkinFont","CodeColor",skinFont.fontColor[MSG_CODE])
+CONFIG_BINDING_REGISTER("SkinFont","OtherColor",skinFont.fontColor[MSG_OTHER])
+CONFIG_BINDING_REGISTER("SkinFont","ActiveMenuColor",skinFont.menuFontColor[MENU_ACTIVE])
+CONFIG_BINDING_REGISTER("SkinFont","InactiveMenuColor",skinFont.menuFontColor[MENU_INACTIVE])
+    
+CONFIG_BINDING_REGISTER("SkinMainBar","BackImg",skinMainBar.backImg)
+CONFIG_BINDING_REGISTER("SkinMainBar","Logo",skinMainBar.logo)
+CONFIG_BINDING_REGISTER("SkinMainBar","Eng",skinMainBar.eng)
+CONFIG_BINDING_REGISTER("SkinMainBar","Active",skinMainBar.active)
+CONFIG_BINDING_REGISTER("SkinMainBar","MarginLeft", skinMainBar.marginLeft)
+CONFIG_BINDING_REGISTER("SkinMainBar","MarginRight", skinMainBar.marginRight)
+CONFIG_BINDING_REGISTER("SkinMainBar","MarginTop", skinMainBar.marginTop)
+CONFIG_BINDING_REGISTER("SkinMainBar","MarginBottom", skinMainBar.marginBottom)
+CONFIG_BINDING_REGISTER_WITH_FILTER("SkinMainBar","Placement", skinMainBar.placement, FilterPlacement)
+
+CONFIG_BINDING_REGISTER("SkinInputBar","BackImg",skinInputBar.backImg)
+CONFIG_BINDING_REGISTER("SkinInputBar","MarginTop", skinInputBar.marginTop)
+CONFIG_BINDING_REGISTER("SkinInputBar","MarginBottom", skinInputBar.marginBottom)
+CONFIG_BINDING_REGISTER("SkinInputBar","MarginLeft", skinInputBar.marginLeft)
+CONFIG_BINDING_REGISTER("SkinInputBar","MarginRight", skinInputBar.marginRight)
+CONFIG_BINDING_REGISTER("SkinInputBar","CursorColor",skinInputBar.cursorColor)
+CONFIG_BINDING_REGISTER("SkinInputBar","InputPos",skinInputBar.iInputPos)
+CONFIG_BINDING_REGISTER("SkinInputBar","OutputPos",skinInputBar.iOutputPos)
+CONFIG_BINDING_REGISTER("SkinInputBar","BackArrow",skinInputBar.backArrow)
+CONFIG_BINDING_REGISTER("SkinInputBar","ForwardArrow",skinInputBar.forwardArrow)
+CONFIG_BINDING_REGISTER("SkinInputBar","BackArrowX",skinInputBar.iBackArrowX)
+CONFIG_BINDING_REGISTER("SkinInputBar","BackArrowY",skinInputBar.iBackArrowY)
+CONFIG_BINDING_REGISTER("SkinInputBar","ForwardArrowX",skinInputBar.iForwardArrowX)
+CONFIG_BINDING_REGISTER("SkinInputBar","ForwardArrowY",skinInputBar.iForwardArrowY)
+
+CONFIG_BINDING_REGISTER("SkinTrayIcon","Active",skinTrayIcon.active)
+CONFIG_BINDING_REGISTER("SkinTrayIcon","Inactive",skinTrayIcon.inactive)
+
+CONFIG_BINDING_REGISTER("SkinMenu", "BackImg", skinMenu.backImg)
+CONFIG_BINDING_REGISTER("SkinMenu", "MarginTop", skinMenu.marginTop)
+CONFIG_BINDING_REGISTER("SkinMenu", "MarginBottom", skinMenu.marginBottom)
+CONFIG_BINDING_REGISTER("SkinMenu", "MarginLeft", skinMenu.marginLeft)
+CONFIG_BINDING_REGISTER("SkinMenu", "MarginRight", skinMenu.marginRight)
+CONFIG_BINDING_REGISTER("SkinMenu", "ActiveColor", skinMenu.activeColor)
+CONFIG_BINDING_REGISTER("SkinMenu", "LineColor", skinMenu.lineColor)
+
+CONFIG_BINDING_REGISTER("SkinKeyboard", "BackImg", skinKeyboard.backImg)
+CONFIG_BINDING_REGISTER("SkinKeyboard", "KeyColor", skinKeyboard.keyColor)
+
+CONFIG_BINDING_END()
+
+void FilterPlacement(GenericConfig* config, ConfigGroup *group, ConfigOption *option, void* value, ConfigSync sync, void* arg)
 {
-    strcpy(key, "// blank");
-    strcpy(value, "// blank");
-}
-
-LoadConfig::LoadConfig() {
-    generalFile=new _GeneralParseStruct [LONG];
-};
-
-LoadConfig::LoadConfig(char *configFilePath) {
-    if ( (configFile=fopen(configFilePath, "r"))==NULL ) {
-        perror("Open file error");
-        exit(1);
+    FcitxSkin* sc = (FcitxSkin*) config;
+    if (sync == Raw2Value)
+    {        
+        ParsePlacement(&sc->skinMainBar.skinPlacement, *(char**) value);
     }
-    generalFile=new _GeneralParseStruct [LONG];
-
-    GeneralParse(configFile, generalFile);
-    ParseSkinInputBar(generalFile, LONG);
-
 }
 
-void LoadConfig::GeneralParse(FILE *configFile, _GeneralParseStruct generalFile[])
+void ParsePlacement(UT_array* sps, char* placment)
 {
-    char tempSave[LONG];
-    char *whereIsEqual;
-    char *whereIsValue;
-
-    int i=0;
-    while ( fgets(tempSave, LONG, configFile)!=NULL ) {
-        if ( tempSave[0]=='\n') {
-            cout << "# blank line" << endl;
+    UT_array* array = SplitString(placment, ';');
+    char** str;
+    utarray_clear(sps);
+    for(str = (char**) utarray_front(array);
+        str != NULL;
+        str = (char**) utarray_next(array, str))
+    {
+        char* s = *str;
+        char* p = strchr(s, ':');
+        if (p == NULL)
             continue;
-        }
-        if ( strchr(tempSave, '#')!=NULL ) {
-            cout << "# commit" << endl;
+        if ((strchr(s, ':') - s) > MAX_STATUS_NAME)
             continue;
-        }
-        whereIsEqual=strchr(tempSave, '=');
-        if ( whereIsEqual==NULL ) {
-            cout << "Null!\n";
-            sscanf(tempSave, " %s ", &generalFile[i].key);
-        } else {
-            // cout << whereIsEqual << endl;
-            *whereIsEqual='\0';
-            whereIsValue=whereIsEqual+sizeof(char);
-            // cout << whereIsValue << endl;
-            strcpy( generalFile[i].key, tempSave);
-            strncpy( generalFile[i].value, whereIsValue, (strlen(whereIsValue)-1) );
-            (generalFile[i].value)[strlen(whereIsValue)-1]='\0';
-        }
-        cout << generalFile[i].key << "\t" << generalFile[i].value << endl;
-        i++;
-    }
-    rewind(configFile);
-}
-
-void LoadConfig::ParseSkinInputBar(_GeneralParseStruct generalFile[], int arrayLenth)
-{
-    int i;
-    for (i=0; i<arrayLenth; i++) {
-        if ( strstr(generalFile[i].key, "[SkinInputBar]")!=NULL ) {
-            break;
-        }
+        
+        int len = p - s;
+        SkinPlacement sp;
+        strncpy(sp.name, s, len);
+        sp.name[len] = '\0';
+        int ret = sscanf(p+1, "%d,%d", &sp.x, &sp.y);
+        if (ret != 2)
+            continue;
+        utarray_push_back(sps, &sp);
     }
     
-    i++;
-    for ( ; ( (strchr(generalFile[i].key, '[')==NULL) && (strchr(generalFile[i].key, '/')==NULL) ); i++ ) {
-        if ( strstr(generalFile[i].key, "BackImg")!=NULL ) {
-            sscanf(generalFile[i].value, "%s", SkinInputBar.BackImg);
+    utarray_free(array);
+}
+
+CONFIG_DESC_DEFINE(GetSkinDesc, "skin.desc")
+
+int LoadSkinConfig(FcitxSkin* sc, const QString& skinType)
+{
+    FILE    *fp;
+    char  buf[PATH_MAX]={0};
+    boolean    isreload = false;
+    int ret = 0;
+
+    snprintf(buf, PATH_MAX, "%s/fcitx_skin.conf", skinType.toLatin1().data());
+    buf[PATH_MAX-1] ='\0';
+    size_t len;
+    char ** path = GetXDGPath(&len, "XDG_CONFIG_HOME", ".config", PACKAGE "/skin" , DATADIR, PACKAGE "/skin" );
+
+    fp = GetXDGFile(buf, path, "r", len, NULL);
+    FreeXDGPath(path);
+
+    if (fp)
+    {
+        ConfigFile *cfile;
+        ConfigFileDesc* skinDesc = GetSkinDesc();
+        if (sc->config.configFile == NULL)
+        {
+            cfile = ParseConfigFileFp(fp, skinDesc);
         }
-        
-        if ( strstr(generalFile[i].key, "MarginLeft")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.MarginLeft);
+        else
+        {
+            cfile = sc->config.configFile;
+            cfile = ParseIniFp(fp, cfile);
         }
-        
-        if ( strstr(generalFile[i].key, "MarginRight")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.MarginRight);
+        if (!cfile)
+        {
+            fclose(fp);
+            fp = NULL;
         }
-        
-        if ( strstr(generalFile[i].key, "MarginTop")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.MarginTop);
-        }
-        
-        if ( strstr(generalFile[i].key, "MarginBottom")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.MarginBottom);
-        }
-        
-        if ( strstr(generalFile[i].key, "CursorColor")!=NULL ){
-            sscanf(generalFile[i].value, "%d %d %d",
-                   &SkinInputBar.CursorColor.r,
-                   &SkinInputBar.CursorColor.g,
-                   &SkinInputBar.CursorColor.b
-                  );
-        }
-        
-        if ( strstr(generalFile[i].key, "InputPos")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.InputPos);
-        }
-        
-        if ( strstr(generalFile[i].key, "OutputPos")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.OutputPos);
-        }
-        
-        if ( strstr(generalFile[i].key, "BackArrow")!=NULL ){
-            sscanf(generalFile[i].value, "%s", &SkinInputBar.BackArrow);
-        }
-        
-        if ( strstr(generalFile[i].key, "ForwardArrow")!=NULL ){
-            sscanf(generalFile[i].value, "%s", SkinInputBar.FowardArrow);
-        }
-        
-        if ( strstr(generalFile[i].key, "BackArrowX")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.BackArrowX);
-        }
-        
-        if ( strstr(generalFile[i].key, "BackArrowY")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.BackArrowY);
-        }
-        
-        if ( strstr(generalFile[i].key, "ForwardArrowX")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.ForwardArrowX);
-        }
-        
-        if ( strstr(generalFile[i].key, "ForwardArrowY")!=NULL ){
-            sscanf(generalFile[i].value, "%d", &SkinInputBar.ForwardArrowY);
+        else
+        {
+            FcitxSkinConfigBind(sc, cfile, skinDesc);
+            ConfigBindSync((GenericConfig*)sc);
         }
     }
-    
+
+    if (!fp)
+        ret = 1;    // 如果安装目录里面也没有配置文件，那就只好告诉用户，无法运行了
+
+    if (fp)
+        fclose(fp);
+
+    return ret;
+
+}
+
+MyLoadConfig::MyLoadConfig(const QString& skinName)
+{
+    memset(&this->skin, 0, sizeof(FcitxSkin));
+    utarray_init(&skin.skinMainBar.skinPlacement, &place_icd);
+    LoadSkinConfig(&this->skin, skinName);
 }
--- src/MyLoadConfig.h
+++ src/MyLoadConfig.h
@@ -2,124 +2,126 @@
 #define MYLOADCONFIG_H
 
 #include <cstdio>
+#include <fcitx-config/fcitx-config.h>
+#include <fcitx/ui.h>
+#include <QString>
 
 
-typedef char shortChar[16];
-typedef char midChar[64];
-typedef char longChar[256];
-
-struct _Color
+typedef struct _SkinInfo
 {
-    int r;
-    int g;
-    int b;
-};
+    char *skinName;
+    char *skinVersion;
+    char *skinAuthor;
+    char *skinDesc;
+} SkinInfo;
 
-struct _GeneralParseStruct
+typedef struct _SkinFont
 {
-    midChar key;
-    midChar value;
+    int fontSize;
+    int menuFontSize;
+    ConfigColor fontColor[7];
+    ConfigColor menuFontColor[2];
+} SkinFont;
+
+typedef struct _SkinMenu
+{
+    char* backImg;
+    int marginTop;
+    int marginBottom;
+    int marginLeft;
+    int marginRight;
+    ConfigColor activeColor;
+    ConfigColor lineColor;
+} SkinMenu;
+
+/**
+ * @brief The Main Window Skin description
+ **/
+typedef struct _SkinMainBar
+{
+    char* backImg;
+    char* logo;
+    char* eng;
+    char* active;
+    int marginTop;
+    int marginBottom;
+    int marginLeft;
+    int marginRight;
+    char *placement;
+    UT_array skinPlacement;
+} SkinMainBar;
+
+typedef struct _SkinInputBar
+{    
+    char* backImg;
+    ConfigColor cursorColor;
+    int marginTop;
+    int marginBottom;
+    int marginLeft;
+    int marginRight;
+    char* backArrow;
+    char* forwardArrow;
+    int iBackArrowX;
+    int iBackArrowY;
+    int iForwardArrowX;
+    int iForwardArrowY;
+    int iInputPos;
+    int iOutputPos;
+} SkinInputBar;
+
+typedef struct _SkinPlacement
+{
+    char name[MAX_STATUS_NAME + 1];
+    int x;
+    int y;
+    UT_hash_handle hh;
+} SkinPlacement;
+
+/**
+ * @brief Tray Icon Image
+ **/
+typedef struct _SkinTrayIcon
+{
+    /**
+     * @brief Active Tray Icon Image
+     **/
+    char* active;
     
-    /** 把key和value都写入“// blank” */
-    _GeneralParseStruct();
-};
+    /**
+     * @brief Inactive Tray Icon Image
+     **/
+    char* inactive;
+} SkinTrayIcon;
 
-struct _SkinInfo
+typedef struct _SkinKeyboard
 {
-    midChar Name;
-    shortChar Version;
-    midChar Author;
-    longChar Desc;
-};
-struct _SkinFont
-{
-    int FontSize;
-    int MenuFontSize;
-    _Color TipColor;
-    _Color InputColor;
-    _Color IndexColor;
-    _Color FirstCandColor;
-    _Color UserPhraseColor;
-    _Color CodeColor;
-    _Color OtherColor;
-    _Color ActiveMenuColor;
-    _Color InactiveMenuColor;
-};
-struct _SkinMainBar
-{
-    midChar BackImg;
-    midChar Logo;
-    midChar Eng;
-    midChar Active;
-    int MarginLeft;
-    int MarginRight;
-    int MarginTop;
-    int MarginBottom;
-};
-struct _SkinInputBar
-{
-    midChar BackImg;
-    int MarginLeft;
-    int MarginRight;
-    int MarginTop;
-    int MarginBottom;
-    _Color CursorColor;
-    int InputPos;
-    int OutputPos;
-    midChar BackArrow;
-    midChar FowardArrow;
-    int BackArrowX;
-    int BackArrowY;
-    int ForwardArrowX;
-    int ForwardArrowY;
-};
-struct _SkinTrayIcon
-{
-    midChar Active;
-    midChar Inactive;
-};
-struct _SkinMenu
-{
-    midChar BackImg;
-    int MarginLeft;
-    int MarginRight;
-    int MarginTop;
-    int MarginBottom;
-    _Color ActiveColor;
-    _Color LineColor;
-};
-struct _SkinKeyboard
-{
-    midChar BackImg;
-    _Color KeyColor;
-};
+    char* backImg;
+    ConfigColor keyColor;
+} SkinKeyboard;
 
-
-class LoadConfig
+/** 
+* 配置文件结构,方便处理,结构固定
+*/
+typedef struct _FcitxSkin
 {
+    GenericConfig config;
+    SkinInfo skinInfo;
+    SkinFont skinFont;
+    SkinMainBar skinMainBar;
+    SkinInputBar skinInputBar;
+    SkinTrayIcon skinTrayIcon;    
+    SkinMenu skinMenu;
+    SkinKeyboard skinKeyboard;
+} FcitxSkin;
+
+CONFIG_BINDING_DECLARE(FcitxSkin);
+
+class MyLoadConfig {
 public:
-    _SkinInfo SkinInfo;
-    _SkinFont SkinFont;
-    _SkinMainBar SkinMainBar;
-    _SkinInputBar SkinInputBar;
-    _SkinTrayIcon SkinTrayIcon;
-    _SkinMenu SkinMenu;
-    _SkinKeyboard SkinKeyboard;
-
-    FILE *configFile;
-    _GeneralParseStruct *generalFile;
-
-    LoadConfig();
-    LoadConfig(char *configFilePath);
+    MyLoadConfig(const QString& skinName);
     
 private:
-    /** 信息读成key-value的形式，返回到tempSave数组里。 */
-    void GeneralParse(FILE *configFile, _GeneralParseStruct generalFile[]); 
-    
-    /** 把tempSave里存的文件读到这个class的SkinInputBar结构中，下面的函数类似。 */
-    void ParseSkinInputBar(_GeneralParseStruct generalFile[], int arrayLenth);
-    
+    FcitxSkin skin;
 };
-
 
 #endif
