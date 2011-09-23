6a9eaa2c75802dfe9fdbfc66f462a6cb8155aa47
Ukyoi D <ukyoi@msn.com>
2011-09-23 20:27:20 +0800

    Now can draw input bar properly (except font?)

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -70,19 +70,11 @@ void MainWindow::DrawAllThings(MyLoadConfig skinClass, QString skinPath)
     QPixmap inputDestPixmap (0, 0);  // The size of this map should be modified by DrawResizableBackground.
     QPixmap mainBarDestPixmap (0, 0); // The size of this map should be modified by DrawMainBar.
     QPixmap menuDestPixmap (0, 0); // The size of this map should be modified by DrawMenu.
-    
+
     DrawInputBar(inputDestPixmap, skinClass.skin, skinPath);
     DrawMainBar(mainBarDestPixmap, skinClass.skin, skinPath);
     DrawMenu(menuDestPixmap, skinClass.skin, skinPath);
 };
-    
-
-/* FIXME:
- * What is it?
-QSize MainWindow::GetInputBarDemoStringSize()
-{
-}
-*/
 
 QColor MainWindow::GetIntColor(ConfigColor floatColor)
 {
@@ -98,7 +90,7 @@ QColor MainWindow::GetIntColor(ConfigColor floatColor)
     switch (b) {
         case 256 : b=255; break;
     }
-    
+
     QColor converted(r, g, b);
     return converted;
 }
@@ -117,10 +109,10 @@ void MainWindow::DrawResizableBackground (
     /**
      * 把一个指定的九宫格图片以resizeWidth和resizeHeight为中央区大小画进destPixmap
      */
-    
+
     int originWidth=backgroundPixmap.width() - marginLeft - marginRight;
     int originHeight=backgroundPixmap.height() - marginTop - marginBottom;
-    
+
     if ( resizeWidth <= 0 )
         resizeWidth = 1;
     if ( resizeHeight <= 0 )
@@ -129,10 +121,10 @@ void MainWindow::DrawResizableBackground (
     destPixmap=QPixmap(resizeWidth + marginLeft + marginRight, resizeHeight + marginTop + marginBottom);
     destPixmap.fill ( Qt::transparent );
     QPainter painter ( &destPixmap );
-    
+
 
     /* 画背景 */
-    
+
     /* 九宫格
      * 7 8 9
      * 4 5 6
@@ -208,7 +200,7 @@ void MainWindow::DrawWidget (
     /**
      * 在指定位置(x,y)按照原大小画出一个小部件，
      */
-    
+
     QPainter painter(&destPixmap);
     painter.drawPixmap( x, y, widgetPixmap );
     painter.end();
@@ -232,15 +224,15 @@ void MainWindow::DrawMainBar(QPixmap &destPixmap, FcitxSkin &skin, QString skinP
     mainBarIcons[11]=( QString(skinPath + "/fullwidth_active.png") );
     mainBarIcons[12]=( QString(skinPath + "/remind_inactive.png") );
     mainBarIcons[13]=( QString(skinPath + "/remind_active.png") );
-    
+
     int marginLeft=skin.skinMainBar.marginLeft;
     int marginRight=skin.skinMainBar.marginRight;
     int marginTop=skin.skinMainBar.marginTop;
     int marginBottom=skin.skinMainBar.marginBottom;
-    
+
     int resizeWidth=0;
     int resizeHeight=0;
-    
+
     for (int i=1; i<MAIN_BAR_ICONS_NUMBER; i++ ) {
         resizeWidth += mainBarIcons[i].width();
     }
@@ -249,30 +241,26 @@ void MainWindow::DrawMainBar(QPixmap &destPixmap, FcitxSkin &skin, QString skinP
             resizeHeight = mainBarIcons[i].height();
         }
     }
-    
+
     int totalWidth=resizeWidth+marginTop+marginBottom;
     int totalHeight=resizeHeight+marginLeft+marginRight;
-    
+
     destPixmap=QPixmap(totalWidth, totalHeight);
-    
+
     DrawResizableBackground(destPixmap, mainBarPixmap,
                             marginLeft, marginRight, marginTop, marginBottom,
                             resizeWidth, resizeHeight
     );
-    
+
     int offset=0;
     for (int i=1; i<MAIN_BAR_ICONS_NUMBER; i++) {
         DrawWidget(destPixmap, mainBarIcons[i], marginLeft + offset, marginTop);
         offset += mainBarIcons[i].width();
     }
-    
+
     mainBarLabel->setPixmap(destPixmap);
     delete [] mainBarIcons;
 }
-
-/* TODO:
-void DrawInputBarDemoString(FcitxSkin &skin, QPixmap &destPixmap, 
-*/
 
 void MainWindow::DrawInputBar(QPixmap &destPixmap, FcitxSkin& skin, QString skinPath)
 {
@@ -280,20 +268,43 @@ void MainWindow::DrawInputBar(QPixmap &destPixmap, FcitxSkin& skin, QString skin
     int marginRight=skin.skinInputBar.marginRight;
     int marginTop=skin.skinInputBar.marginTop;
     int marginBottom=skin.skinInputBar.marginBottom;
-    int resizeWidth=180;
-    int resizeHeight=40;
-    
+
+    // Draw Demo string:
+    QString inputExample(tr("input example"));
+    QString numberStr[3];
+    QString candStr[3];
+    for (int i=0; i<3; i++) {
+        numberStr[i]=QString("%1.").arg(i+1);
+    }
+    candStr[0]=QString(tr("First candidate"));
+    candStr[1]=QString(tr("User phrase"));
+    candStr[2]=QString(tr("Others"));
+    int offset=marginLeft;
+
+    QFont inputFont("");
+    int fontHeight=skin.skinFont.fontSize;
+    QFontMetrics metrics(inputFont);
+    inputFont.setPixelSize(fontHeight);
+
     QPixmap inputBarPixmap( QString(skinPath + '/' + skin.skinInputBar.backImg) );
+    int resizeWidth=0;
+    int resizeHeight=2*fontHeight+3;
+    for (int i=0; i<3; i++) {
+        resizeWidth+=metrics.width(numberStr[i]);
+        qDebug() << resizeWidth;
+        resizeWidth+=metrics.width(candStr[i]);
+        qDebug() << resizeWidth;
+    };
     int totalWidth=marginLeft + marginRight + resizeWidth;
     int totalHeight=marginTop + marginBottom + resizeHeight;
+
     destPixmap=QPixmap(totalWidth, totalHeight);
     destPixmap.fill(Qt::transparent);
-    QPainter painter(&inputBarPixmap);
     DrawResizableBackground(destPixmap, inputBarPixmap,
                             marginLeft, marginRight, marginTop, marginBottom,
                             resizeWidth, resizeHeight
     );
-    
+
     QPixmap backArrowPixmap( QString(skinPath + '/' + skin.skinInputBar.backArrow) );
     QPixmap forwardArrowPixmap( QString(skinPath + '/' + skin.skinInputBar.forwardArrow) );
     DrawWidget(destPixmap, backArrowPixmap,
@@ -302,6 +313,44 @@ void MainWindow::DrawInputBar(QPixmap &destPixmap, FcitxSkin& skin, QString skin
     DrawWidget(destPixmap, forwardArrowPixmap,
                totalWidth - skin.skinInputBar.iForwardArrowX, skin.skinInputBar.iForwardArrowY
     );
+
+    QPainter textPainter(&destPixmap);
+    textPainter.setFont(inputFont);
+
+    QColor tipColor=GetIntColor(skin.skinFont.fontColor[0]);
+    QColor inputColor=GetIntColor(skin.skinFont.fontColor[1]);
+    QColor indexColor=GetIntColor(skin.skinFont.fontColor[2]);
+    QColor firstCandColor=GetIntColor(skin.skinFont.fontColor[3]);
+    QColor userPhraseColor=GetIntColor(skin.skinFont.fontColor[5]);
+    QColor codeColor=GetIntColor(skin.skinFont.fontColor[5]);
+    QColor otherColor=GetIntColor(skin.skinFont.fontColor[6]);
+
+
+    textPainter.setPen(inputColor);
+    textPainter.drawText(marginLeft, marginTop, resizeWidth, fontHeight, Qt::AlignLeft, inputExample);
+
+    textPainter.setPen(indexColor);
+    for (int i=0; i<3; i++) {
+        textPainter.drawText(offset, marginTop+3+fontHeight, resizeWidth, resizeHeight, Qt::AlignLeft, numberStr[i]);
+        offset=offset + metrics.width(numberStr[i]) + metrics.width(candStr[i]);
+    }
+
+    offset=marginLeft+metrics.width(numberStr[0]);
+
+    textPainter.setPen(firstCandColor);
+    textPainter.drawText(offset, marginTop+3+fontHeight, resizeWidth, resizeHeight, Qt::AlignLeft, candStr[0]);
+    offset=offset+metrics.width(candStr[0])+metrics.width(numberStr[1]);
+
+    textPainter.setPen(userPhraseColor);
+    textPainter.drawText(offset, marginTop+3+fontHeight, resizeWidth, resizeHeight, Qt::AlignLeft, candStr[1]);
+    offset=offset+metrics.width(candStr[1])+metrics.width(numberStr[2]);
+
+    textPainter.setPen(otherColor);
+    textPainter.drawText(offset, marginTop+3+fontHeight, resizeWidth, resizeHeight, Qt::AlignLeft, candStr[2]);
+
+
+    textPainter.end();
+
     inputWindowLabel->setPixmap(destPixmap);
 }
 
@@ -312,55 +361,59 @@ void MainWindow::DrawMenu(QPixmap &destPixmap, FcitxSkin &skin, QString skinPath
     int marginRight=skin.skinMenu.marginRight;
     int marginTop=skin.skinMenu.marginTop;
     int marginBottom=skin.skinMenu.marginBottom;
-    int width=100;
-    int height=100;
     int offset=marginTop;
     QPixmap backgroundPixmap( QString(skinPath + '/' + skin.skinMenu.backImg) );
     QColor menuFontColor0( GetIntColor(skin.skinFont.menuFontColor[0]) );
     QColor menuFontColor1( GetIntColor(skin.skinFont.menuFontColor[1]) );
-    qDebug() << menuFontColor0;
-    qDebug() << menuFontColor1;
     QColor activeColor( GetIntColor(skin.skinMenu.activeColor) );
     QColor lineColor( GetIntColor(skin.skinMenu.lineColor) );
-    
-    // FIXME:
-    // Color is double type!
-    
-    DrawResizableBackground(destPixmap, backgroundPixmap,
-                            marginLeft, marginRight, marginTop, marginBottom,
-                            width, height
-    );
-    
-    QPainter textPainter(&destPixmap);
+
+    QString text1(tr("Selected line"));
+    QString text2(tr("Unselected line"));
+    QString text3(tr("Fcitx Skin Viewer"));
+
     int fontSize=skin.skinFont.menuFontSize;
     QFont menuFont("");
     menuFont.setPixelSize(fontSize);
-    
+    QFontMetrics metrics(menuFont);
+
+    int resizeWidth=metrics.width(text3)+6;
+    int resizeHeight=100;
+
+    DrawResizableBackground(destPixmap, backgroundPixmap,
+                            marginLeft, marginRight, marginTop, marginBottom,
+                            resizeWidth, resizeHeight
+    );
+
+    QPainter textPainter(&destPixmap);
     textPainter.setPen(activeColor);
-    textPainter.fillRect(marginLeft, marginTop, width, fontSize, activeColor );
-    
+    textPainter.fillRect(marginLeft, marginTop, resizeWidth, fontSize, activeColor );
+
     //Draw text.
     textPainter.setFont( menuFont );
     textPainter.setPen(menuFontColor0);
-    textPainter.drawText(marginLeft, offset, width, fontSize, Qt::AlignCenter, QString::fromUtf8("选中的行") );
+    textPainter.drawText(marginLeft, offset, resizeWidth, fontSize, Qt::AlignCenter, text1);
     SET_OFFSET
-    
+
     textPainter.setPen(menuFontColor1);
-    textPainter.drawText(marginLeft, offset, width, fontSize, Qt::AlignCenter, QString::fromUtf8("未选中的行") );
+    textPainter.drawText(marginLeft, offset, resizeWidth, fontSize, Qt::AlignCenter, text2);
     SET_OFFSET
-    
+
     //Draw a line.
     textPainter.setPen(lineColor);
-    textPainter.fillRect(marginLeft+3, offset, width-6, 2, lineColor);
+    textPainter.fillRect(marginLeft+3, offset, resizeWidth-6, 2, lineColor);
     offset+=5;
-    
+
     textPainter.setPen(menuFontColor1);
-    textPainter.drawText(marginLeft, offset, width, fontSize, Qt::AlignCenter, QString::fromUtf8("Fcitx皮肤查看器") );
+    textPainter.drawText(marginLeft, offset, resizeWidth, fontSize, Qt::AlignCenter, text3);
     SET_OFFSET
-    
+
+    textPainter.end();
+
     menuLabel->setPixmap(destPixmap);
-    
+
 }
 
 
 #include "MainWindow.moc"
+
